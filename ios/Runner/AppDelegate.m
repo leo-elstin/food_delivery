9954394478c7c88b664265cd7fd844dfb91aab24
Leo Elstin <leoelstin@gmail.com>
2019-04-19 20:02:22 +0530

    fixes for iOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,12 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
-#import "GoogleMaps/GoogleMaps.h"
+// #import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
 didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [GMSServices provideAPIKey:@"AIzaSyC-UZW-0_xR9GsMbUxJXb2nbhnyRkT_m5Q"];
+    // [GMSServices provideAPIKey:@"AIzaSyC-UZW-0_xR9GsMbUxJXb2nbhnyRkT_m5Q"];
     [GeneratedPluginRegistrant registerWithRegistry:self];
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
