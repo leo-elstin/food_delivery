197fcaa221fba8f0df2cb761187885815b6a2cf9
Leo Elstin <leoelstin@gmail.com>
2019-04-22 18:12:32 +0530

    map integrated with polylines

--- lib/pages/order_tracking_page.dart
+++ lib/pages/order_tracking_page.dart
@@ -1,52 +1,149 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:io' show Platform;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class OrderTrackingPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _OrderTrackingState();
+    return PlacePolylineBodyState();
   }
 }
 
+class PlacePolylineBodyState extends State<OrderTrackingPage> {
+  PlacePolylineBodyState();
 
-class _OrderTrackingState extends State<OrderTrackingPage> {
   Completer<GoogleMapController> _controller = Completer();
+  Map<PolylineId, Polyline> polylines = <PolylineId, Polyline>{};
+  int _polylineIdCounter = 1;
+  PolylineId selectedPolyline;
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  // Values when toggling polyline color
+  int colorsIndex = 0;
+  List<Color> colors = <Color>[
+    Colors.purple,
+    Colors.red,
+    Colors.green,
+    Colors.pink,
+  ];
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  // Values when toggling polyline width
+  int widthsIndex = 0;
+  List<int> widths = <int>[10, 20, 5];
+
+  int jointTypesIndex = 0;
+  List<JointType> jointTypes = <JointType>[
+    JointType.mitered,
+    JointType.bevel,
+    JointType.round
+  ];
+
+  // Values when toggling polyline end cap type
+  int endCapsIndex = 0;
+  List<Cap> endCaps = <Cap>[Cap.buttCap, Cap.squareCap, Cap.roundCap];
+
+  // Values when toggling polyline start cap type
+  int startCapsIndex = 0;
+  List<Cap> startCaps = <Cap>[Cap.buttCap, Cap.squareCap, Cap.roundCap];
+
+  // Values when toggling polyline pattern
+  int patternsIndex = 0;
+  List<List<PatternItem>> patterns = <List<PatternItem>>[
+    <PatternItem>[],
+    <PatternItem>[
+      PatternItem.dash(30.0),
+      PatternItem.gap(20.0),
+      PatternItem.dot,
+      PatternItem.gap(20.0)
+    ],
+    <PatternItem>[PatternItem.dash(30.0), PatternItem.gap(20.0)],
+    <PatternItem>[PatternItem.dot, PatternItem.gap(10.0)],
+  ];
+
+  void _onMapCreated(GoogleMapController controller) {
+    // this._controller = controller;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void _onPolylineTapped(PolylineId polylineId) {
+    setState(() {
+      selectedPolyline = polylineId;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        onTap: (value){
-          
-        },
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+    final bool iOSorNotSelected = Platform.isIOS || (selectedPolyline == null);
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addGymPoly();
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      body: GoogleMap(
+        myLocationEnabled: true,
+        onTap: (value) {
+          print(value);
+        },
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(-8.913025, 13.202462),
+          zoom: 7.0,
+        ),
+        polylines: Set<Polyline>.of(polylines.values),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+          //_initCameraPosition();
+        },
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  void _addGymPoly() {
+    _gymCameraPosition();
+    final String polylineIdVal = 'polyline_id_$_polylineIdCounter';
+    _polylineIdCounter++;
+    final PolylineId polylineId = PolylineId(polylineIdVal);
+
+    final Polyline polyline = Polyline(
+      polylineId: polylineId,
+      consumeTapEvents: true,
+      color: Colors.black,
+      width: 5,
+      points: _createGymPoints(),
+      onTap: () {
+        _onPolylineTapped(polylineId);
+      },
+    );
+
+    setState(() {
+      polylines[polylineId] = polyline;
+    });
+  }
+
+  Future<void> _gymCameraPosition() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: LatLng(-8.912014, 13.204197),
+      zoom: 18.5,
+    )));
+  }
+
+  List<LatLng> _createGymPoints() {
+    final List<LatLng> points = <LatLng>[];
+    points.add(_createLatLng(-8.911857, 13.203656));
+    points.add(_createLatLng(-8.911580, 13.204369));
+    points.add(_createLatLng(-8.912060, 13.204649));
+    points.add(_createLatLng(-8.912406, 13.204128));
+    // points.add(_createLatLng(-8.911857, 13.203656));
+    return points;
+  }
+
+  LatLng _createLatLng(double lat, double lng) {
+    return LatLng(lat, lng);
   }
 }
